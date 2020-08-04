f9b47ccc8978bcce62dbb3f2e0b8469491f10df4
钱园园 <qyy197@zcsmart.com>
2020-08-04 23:24:24 +0800

    add 增加用例

---
+++ test/equal.test.js
@@ -0,0 +1,30 @@
+const toFixedScale = require('../lib/number.js')
+
+test('6.1234保留两位小数，等于数字6.12', () => {
+  expect(toFixedScale(6.1234, 2)).toBe('6.12')
+})
+
+
+// test('两个数组相等:toBe', () => {
+//   const a = [1,2,3]
+//   const b = [1,2,3]
+//   expect(a).toBe(b)
+// })
+
+test('两个数组相等:toEqual', () => {
+  const a = [1,2,3]
+  const b = [1,2,3]
+  expect(a).toEqual(b)
+})
+
+test('两个对象相等:toEqual', () => {
+  const a = {
+    name: 'animal',
+    eat: function () {}
+  }
+  const b = {
+    name: 'animal',
+    eat: function () {}
+  }
+  expect(a).toEqual(b)
+})
---
+++ test/equal1.test.js
@@ -0,0 +1,4 @@
+test('0是否为null', () => {
+  const zero = 0
+  expect(zero).toBeNull()
+})
