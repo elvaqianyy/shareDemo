972995a7eb00fa70cce2c8a9f381985b9b87003f
yuanyuan.qian <yuanyuan.qian@yintech.cn>
2020-08-10 17:36:23 +0800

    update

--- test/equal1.test.js
+++ test/equal1.test.js
@@ -1,4 +1,13 @@
+import '../lib/boolean'
 test('0是否为null', () => {
   const zero = 0
   expect(zero).toBeNull()
+})
+test.only('链接是否为百度', () => {
+  const item = {
+    url: 'https://www.baidu.com',
+    title: 'some-title'
+  }
+  const href = document.querySelector('.box a').getAttribute('href')
+  expect(href === item.url).toBe(true)
 })
